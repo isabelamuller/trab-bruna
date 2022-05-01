a53209677baa709db61866e6125e743b0cb84912
Isabela Müller <isabelamuller1@hotmail.com>
2022-05-01 20:44:40 -0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        Equipe equipe1 = new Equipe("marquinhos futebol", 3, 1);
-        Equipe equipe2 = new Equipe("karol conka basquete", 4, 0);
-        Equipe equipe3 = new Equipe("lucas penteado satanismo", 1, 3);
+        Equipe equipe1 = new Equipe("Cruzeiro", 3, 1);
+        Equipe equipe2 = new Equipe("Santos FC", 4, 0);
+        Equipe equipe3 = new Equipe("Flamengo", 1, 3);
 
         System.out.println("A equipe " + equipe1.getNomeEquipe() + " obteve a pontuação " + equipe1.calculaPontuacao());
         System.out.println("A equipe " + equipe2.getNomeEquipe() + " obteve a pontuação " + equipe2.calculaPontuacao());
